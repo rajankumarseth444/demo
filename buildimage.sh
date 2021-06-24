4e419264cf0c73200e9b6f091dbd674549cd8f83
Rajan Kumar Seth <57744099+rajankumarseth444@users.noreply.github.com>
2021-06-24 10:29:11 +0530

    Update buildimage.sh
    Just updated fresh

--- buildimage.sh
+++ buildimage.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 # This is an Sample project to build BaseImage needed for CICD of
 # JAVA Web Application used for Wezva Technologies
-# Author: Adam M
-# Email: semlearningcentre@gmail.com
-# Phone: 9739710917 # www.wezva.com
+# Author: Adam M        # Email: semlearningcentre@gmail.com
+# Phone: 9739710917     # www.wezva.com
 
 # Setup logging
 log()
 {
-echo -e "[date '+% Y-%m-%d %T'']:" $1
+          echo -e "[ `date '+% Y-%m-%d %T'`]:" $1
 }
-build_image)
+build_image()
 {
     log "INFO: Starting docker build of $1"
 
     /usr/bin/docker build --build-arg BASE_IMAGE_NAME=${BASE_IMAGE} -t ${Build_Img} . --no-cache --force-rm
-    if [ $? -ne "0" 1; then
+    if [ $? -ne "0" ]; then
       log "ERROR: Docker build failed"
       exit
     fi
@@ -33,13 +32,13 @@ push_image()
        log "ERROR: Docker registry login failed"
        exit 1
      else
-        log "INFO: Docker registry login [[ success ]]"
+        log "INFO: Docker registry login [[ success ]] "
         fi
 
 
         log "INFO: Starting to push image to the repository"
         /usr/bin/docker tag ${Build_Img} adamtravis/${Build_Img}
-        /usr/bin/docker push adamtravis/${Buildimg}
+        /usr/bin/docker push adamtravis/${Build_Img}
 
         if [ $? -ne "0" ]; then
             log "ERROR: Docker Push command failed"
@@ -55,14 +54,13 @@ push_image()
         if [ "$#" -eq 3 ]; then
            BASE_IMAGE=$1
            Build_Img=$2
-           pasw=$3
+           paswd=$3
         else
-          BASE_IMAGE="jboss/base-jdk: 11"
+          BASE_IMAGE="jboss/base-jdk:11"
           Build_Img="wildfly"
         fi
 
 
 build_image
 push_image
-
 
